1c7f11cdf9eeb2c4bb1e50b011fe69c66e8b07c4
Chance Shiflett <chanceshiflett@yahoo.com>
2016-02-10 19:04:07 -0800

    Remove addStringElement method

--- src/main/java/com/sqa/cs/util/helper/RequestInput.java
+++ src/main/java/com/sqa/cs/util/helper/RequestInput.java
@@ -7,7 +7,6 @@
  */
 package com.sqa.cs.util.helper;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -25,27 +24,6 @@ public class RequestInput {
 
 	// Scanner object
 	public static Scanner keyboard = new Scanner(System.in);
-
-	// Create a method that takes a String array and increases it's value by one
-	// element
-	public static String[] addStringElement(String[] elements, String addString) {
-		// local variables
-		String[] newStringArray;
-		// Check if the supplied String array is null and if so, we will be
-		// adding one element
-		if (elements != null) {
-			// supplied array contains elements so the new size will be increase
-			// by one
-			newStringArray = Arrays.copyOf(elements, elements.length + 1);
-			// Set the last element to supplied String addString
-			newStringArray[elements.length] = addString;
-		} else {
-			// Supplied array does not contain any elements yet
-			newStringArray = new String[1];
-			newStringArray[0] = addString;
-		}
-		return newStringArray;
-	}
 
 	/**
 	 * Static helper method asks the user a specified question and as long as
